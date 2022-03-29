aff228f2d2f80f495cccc25903835d2a2210e3ec
MiriamPoveda <mpoveda94@gmail.com>
2022-03-29 09:39:27 +0200

    Add number choice function (+ sms)

--- js/main.js
+++ js/main.js
@@ -2,11 +2,13 @@
 
 // CONSTANTES //
 
-const select = document.querySelector('js_select');
-const number = document.querySelector('js_number');
-const button = document.querySelector('js_button');
-const sms = document.querySelector('js_sms');
-const money = document.querySelector('js_money');
+const number = document.querySelector('.js_number');
+const select = document.querySelector('.js_select');
+const button = document.querySelector('.js_button');
+const sms = document.querySelector('.js_sms');
+const money = document.querySelector('.js_money');
+
+const random = getRandomNumber(6);
 
 // FUNCIONES Y CONDICIONALES //
 
@@ -15,13 +17,26 @@ const money = document.querySelector('js_money');
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
    }
-   
+
+/* Seleccionar número */
+
+function chooseNumber() {
+const selectNumber = parseInt(select.value);
+
+if (selectNumber === random) {
+    sms.innerHTML = "¡Has ganado el doble de lo apostado!"
+} else {
+    sms.innerHTML = "¡Oh! Has perdido lo apostado."
+}
+}
+
 /* Función base */
 
 function handleClickButton(event) {
     event.preventDefault();
+    chooseNumber();
 }
 
 // EVENTOS // 
 
-button.addEventListener('click', handleClickButton);
+button.addEventListener("click", handleClickButton);
