3b26e9a395da6492eff1330b4c093b58444cc1d7
MiriamPoveda <mpoveda94@gmail.com>
2022-03-29 09:01:20 +0200

    Add consts, event and initial functions

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
 'use strict';
 
-console.log('Holis');
+// CONSTANTES //
+
+const select = document.querySelector('js_select');
+const number = document.querySelector('js_number');
+const button = document.querySelector('js_button');
+const sms = document.querySelector('js_sms');
+const money = document.querySelector('js_money');
+
+// FUNCIONES Y CONDICIONALES //
+
+/* Número aleatorio */
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+   }
+   
+/* Función base */
+
+function handleClickButton(event) {
+    event.preventDefault();
+}
+
+// EVENTOS // 
+
+button.addEventListener('click', handleClickButton);
